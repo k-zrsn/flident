45feb22f4b3542df69fc5dcccad64b7ca3affd43
vpadua <vgcpad@gmail.com>
2025-07-17 17:09:34 -0400

    create variables

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ from FlightRadar24.api import FlightRadar24API
 
 
 
+### Create variables
+blank = ["N/A", "", "NONE"]
+max_flights = 10
+min_alt = 100 #ft
+max_alt = 20000 #ft
+location = [
+    38.977903, #lat
+    -77.006570, #lon
+    0.0859536 #elevation km
+]
+radius = {
+    "tl_y": 39.100858, # top left lat 
+    "tl_x": -77.232313, # top left lon
+    "br_y": 38.645774, # bottom right lat
+    "br_x": -76.695424 # bottom right lon
+}
+
+
+
 
